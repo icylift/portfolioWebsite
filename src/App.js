ba5b2be13a1eadc65cbf7e8ab5ce5e8646233bc6
Aaron Williams <polarizedcuriosity@gmail.com>
2023-01-17 16:38:34 -0800

    added NavBar component, and added some css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Header />
       <Body />
     </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+function NavBar(){
+  return(
+    <div>
+      <div className='navBar'>
+        <div>
+          <h1>AW Logo Here!</h1>
+        </div>
+        <div className='navBarButtonDiv'>
+          <button className='navBarButtons'>Home</button>
+          <button className='navBarButtons'>Portfolio</button>
+          <button className='navBarButtons'>Contact Me</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+
+export default NavBar 
