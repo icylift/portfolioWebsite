c70a00606ba919388d361d2f673581af7e9d039b
Aaron Williams <polarizedcuriosity@gmail.com>
2023-01-19 21:13:25 -0800

    Made some changes in the contact form Component.

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -6,15 +6,23 @@ function ContactMe(){
   return(
     <div>
       <div className='ContactDiv'>
-        <h1>Contact Form</h1>
+        <h1>Contact Me</h1>
         <form className='contactForm'>
-          <h3>Full name</h3>
+
+
+          <h3>Name</h3>
           <input type="text"/>
+
+
           <h3>Email</h3>
           <input type="text"/>
+
+
           <h3>Phone Contact</h3>
           <input type="text"/>
-          <button>Submit</button>
+
+
+          <button className='contactButton'>Submit</button>
         </form>
       </div>
     </div>
