711823c231c56125d3bb1fed9993f59960370f1b
Aaron Williams <polarizedcuriosity@gmail.com>
2023-01-20 12:42:50 -0800

    Added a portfolio component and did some css in contactMe and Skills div

---
+++ src/components/Portfolio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function Portfolio(){
+  return(
+    <div>
+      <div className=''>
+        <h1 className=''></h1>
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+
+export default Portfolio 
