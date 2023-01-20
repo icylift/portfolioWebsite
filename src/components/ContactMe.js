ea3f04db8a6ab5a4586769e31470f189c99ce022
Aaron Williams <polarizedcuriosity@gmail.com>
2023-01-19 17:17:49 -0800

    Added css to contact dive, also added some more elements

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -7,9 +7,12 @@ function ContactMe(){
     <div>
       <div className='ContactDiv'>
         <h1>Contact Form</h1>
-        <form>
+        <form className='contactForm'>
+          <h3>Full name</h3>
           <input type="text"/>
+          <h3>Email</h3>
           <input type="text"/>
+          <h3>Phone Contact</h3>
           <input type="text"/>
           <button>Submit</button>
         </form>
