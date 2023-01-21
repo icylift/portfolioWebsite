87b989c1b3285345615b766eb038cec6de40c73b
Aaron Williams <polarizedcuriosity@gmail.com>
2023-01-20 22:18:22 -0800

    added a new div called greetingDiv, also added css to center that div. Then I added css to make the container a darker grey color.

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,14 +4,15 @@ import '../App.css';
 
 function Body(){
   return(
-    <div>
-      <div className='content'>
-        <div className='bigGreeting'>Hello</div>
-        <p>
-          My name is Aaron, 
-        </p>
+    <div className='body'>
+      <div className='greetingDiv'>
+        <div className='content'>
+          <div className='bigGreeting'>Hello</div>
+            <p>
+              My name is Aaron, 
+            </p>
+        </div>
       </div>
-      
       <div className='resumePortfolioButtons'>
         <button>Resume</button>
         <button>Portfolio</button>
@@ -20,20 +21,19 @@ function Body(){
       <div className='skillsDiv'>
         <h1 className='skillsTitle'>Skills</h1>
         <div className='skillListDiv'>
-        <ul className='skillList'>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>Javascript</li>
-          <li>react</li>
-        </ul>
-        <ul className='skillList2'>
-          <li>Express</li>
-          <li>MongoDB</li>
-          <li>Git / Git Hub</li>
-          <li>Agile / Scrum</li>
-        </ul>
+          <ul className='skillList'>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Javascript</li>
+            <li>react</li>
+          </ul>
+          <ul className='skillList2'>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>Git / Git Hub</li>
+            <li>Agile / Scrum</li>
+          </ul>
         </div>
-
       </div>
     </div>
   )
