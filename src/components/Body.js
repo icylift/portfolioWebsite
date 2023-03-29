491552e0a45bb2b407b40aa2667067152383b23e
Aaron Williams <polarizedcuriosity@gmail.com>
2023-03-29 10:04:39 -0700

    added a description off me

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,32 +1,40 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
-
-function Body(){
-  return(
-    <div className='body'>
-      <div className='greetingDiv'>
-        <div className='content'>
-          <div className='bigGreeting'>Hello</div>
-            <p>
-              My name is Aaron, 
-            </p>
+function Body() {
+  return (
+    <div className="body">
+      <div className="greetingDiv">
+        <div className="content">
+          <div className="bigGreeting">Hello</div>
+          <p>
+            My name is Aaron, <br /> I am a dedicated and driven individual who
+            is eager to prove my worth through hard work and a strong work
+            ethic. I am constantly seeking new challenges to push my skills to
+            the limit, and I have a deep passion for technology and building
+            visually stunning projects. My commitment to excellence and
+            attention to detail ensures that every project I work on is of the
+            highest quality, and I take pride in delivering exceptional results
+            that exceed my clients' expectations.
+          </p>
         </div>
       </div>
-      <div className='centerContactButtonDiv'>
-        <button className='centerContactBtn'><span>Contact</span></button>
+      <div className="centerContactButtonDiv">
+        <button className="centerContactBtn">
+          <span>Contact</span>
+        </button>
       </div>
 
-      <div className='skillsDiv'>
-        <h1 className='skillsTitle'>Skills</h1>
-        <div className='skillListDiv'>
-          <ul className='skillList'>
+      <div className="skillsDiv">
+        <h1 className="skillsTitle">Skills</h1>
+        <div className="skillListDiv">
+          <ul className="skillList">
             <li>HTML5</li>
             <li>CSS3</li>
             <li>Javascript</li>
             <li>react</li>
           </ul>
-          <ul className='skillList2'>
+          <ul className="skillList2">
             <li>Express</li>
             <li>MongoDB</li>
             <li>Git / Git Hub</li>
@@ -35,12 +43,7 @@ function Body(){
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-
-
-
-
-
-export default Body 
+export default Body;
